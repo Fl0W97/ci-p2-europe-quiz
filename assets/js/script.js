65acc3f3a02dce517f1b30a8de03e16a8950499a
Florian Guldner <florian.guldner@googlemail.com>
2024-04-14 12:37:25 +0000

    add first variables etc. to js

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+/* country-capital database as object */
+let allCountriesCapitals = {
+    Germany: 'Berlin',
+    France: 'Paris',
+    Poland: 'Warschauw',
+    Italy: 'Rom',
+    Spain: 'Madrid',
+    Portugal: 'Lissabon',
+    Austria: 'Wien',
+    Swizerland: 'Bern',
+    Netherlands: 'Amsterdam',
+}
+
+/* variables */
+let user
+
+let QuestionCity
+
+let QuestionCountry
+
+let MultiplyChoiceCity1
+
+let MultiplyChoiceCity2
+
+let MultiplyChoiceCountry1
+
+let MultiplyChoiceCountry2
+
+let highscore
+
+
+
+/* provides the questions */
+function randomQuestion ()
+
+/* checks answer */
+function checkAnswer ()
+
+function wrongAnswer ()
+
+function timer ()
+
+function SaveUser()
+
+function countHighscore ()
+
+function gameModeStart()
+
+function gameModeMultiplyChoice()
+
+function gameModeJoker()
