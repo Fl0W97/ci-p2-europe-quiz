f0761b2aac93f8e821c7d1c8f3855a9044123c82
Florian Guldner <florian.guldner@googlemail.com>
2024-05-28 19:42:05 +0000

    update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,149 @@
-/* country-capital database as nested list*/
-let allCountriesCapitals = {
-    country: ['Albania', 'Andorra', 'Austria', 'Belarus', 'Belgium', 'Bosnia and Herzegovina', 'Bulgaria', 'Croatia', 'Cyprus', 'Czech Republic', 'Denmark', 'Estonia', 'Finland', 'France', 'Georgia', 'Germany', 'Gibraltar (UK)', 'Greece', 'Hungary', 'Iceland', 'Ireland', 'Italy', 'Kosovo', 'Lativa', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macedonia', 'Malta', 'Moldova', 'Monaco', 'Montenegro', 'Netherlands', 'Norway', 'Poland', 'Portugal', 'Romania', 'Russia', 'San Marino', 'Serbia', 'Slovakia', 'Slovenia', 'Spain', 'Sweden', 'Switzerland', 'Turkey', 'Ukraine', 'United Kingdom', 'Vatican City'],
-    capital: ['Tirana', 'Andorra la Vella', 'Vienna', 'Minsk', 'Brussels', 'Sarajevo', 'Sofia', 'Zagreb', 'Nicosia', 'Prague', 'Copenhagen', 'Tallin', 'Helsinki', 'Paris', 'Tbilisi', 'Berlin', 'Gibraltar', 'Athens', 'Budapest', 'Reykjavik', 'Dublin', 'Rome', 'Pristina', 'Riga', 'Vaduz', 'Vulnius', 'Luxembourg city', 'Skopje', 'Valletta', 'Chisinau', 'Monaco', 'Podgorica', 'Amsterdam', 'Oslo' , 'Warsaw', 'Lisbon', 'Bucharest', 'Moscow', 'San Marino', 'Belgrade', 'Bratislava', 'Ljubljana', 'Madrid', 'Stockholm', 'Bern', 'Ankara', 'Kiev', 'London', 'Vatican City']
-};
+// Country-capital database as a nested list
+const CountriesCapitals = [
+    { country: "Albania", capital: "Tirana" },
+    { country: "Andorra", capital: "Andorra la Vella" },
+    { country: "Austria", capital: "Vienna" },
+    { country: "Belarus", capital: "Minsk" },
+    { country: "Belgium", capital: "Brussels" },
+    { country: "Bosnia and Herzegovina", capital: "Sarajevo" },
+    { country: "Bulgaria", capital: "Sofia" },
+    { country: "Croatia", capital: "Zagreb" },
+    { country: "Cyprus", capital: "Nicosia" },
+    { country: "Czech Republic", capital: "Prague" },
+    { country: "Denmark", capital: "Copenhagen" },
+    { country: "Estonia", capital: "Tallin" },
+    { country: "Finland", capital: "Helsinki" },
+    { country: "France", capital: "Paris" },
+    { country: "Georgia", capital: "Tbilisi" },
+    { country: "Germany", capital: "Berlin" },
+    { country: "Gibraltar (UK)", capital: "Gibraltar" },
+    { country: "Greece", capital: "Athens" },
+    { country: "Hungary", capital: "Budapest" },
+    { country: "Iceland", capital: "Reykjavik" },
+    { country: "Ireland", capital: "Dublin" },
+    { country: "Italy", capital: "Rome" },
+    { country: "Kosovo", capital: "Pristina" },
+    { country: "Latvia", capital: "Riga" },
+    { country: "Liechtenstein", capital: "Vaduz" },
+    { country: "Lithuania", capital: "Vilnius" },
+    { country: "Luxembourg", capital: "Luxembourg City" },
+    { country: "Macedonia", capital: "Skopje" },
+    { country: "Malta", capital: "Valletta" },
+    { country: "Moldova", capital: "Chisinau" },
+    { country: "Monaco", capital: "Monaco" },
+    { country: "Montenegro", capital: "Podgorica" },
+    { country: "Netherlands", capital: "Amsterdam" },
+    { country: "Norway", capital: "Oslo" },
+    { country: "Poland", capital: "Warsaw" },
+    { country: "Portugal", capital: "Lisbon" },
+    { country: "Romania", capital: "Bucharest" },
+    { country: "Russia", capital: "Moscow" },
+    { country: "San Marino", capital: "San Marino" },
+    { country: "Serbia", capital: "Belgrade" },
+    { country: "Slovakia", capital: "Bratislava" },
+    { country: "Slovenia", capital: "Ljubljana" },
+    { country: "Spain", capital: "Madrid" },
+    { country: "Sweden", capital: "Stockholm" },
+    { country: "Switzerland", capital: "Bern" },
+    { country: "Turkey", capital: "Ankara" },
+    { country: "Ukraine", capital: "Kiev" },
+    { country: "United Kingdom", capital: "London" },
+    { country: "Vatican City", capital: "Vatican City" }
+];
 
-//Manage highscore, list not working yet !!!!
-let highscoreList = {
-    user: [],
-    highscore: []
+// User input relevant for modeTypeAnswer = userInput
+let userName = document.getElementById('username');
+let numberQuestions = parseInt(document.getElementById('noOfQuestions').value, 10);
+let userInput;
+
+// Boxes for hide/display
+let box_answers = document.getElementById("box_answers");
+
+// Initialize variables/constants
+let index = 0; // Initialize index outside the function
+let modeTypeQuestion;
+let wrongAnswer;
+let correctAnswer;
+let userAnswer;
+
+// Answers multiple choice
+const answer1 = document.getElementById("box1");
+const answer2 = document.getElementById("box2");
+const answer3 = document.getElementById("box3");
+
+// Highscore
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+let highscore;
+
+// DOM load
+document.addEventListener("DOMContentLoaded", function () {
+    // Add event listener to the 'Start Quiz' button
+    const startQuizButton = document.getElementById('button');
+    startQuizButton.addEventListener('click', startQuiz);
+
+    document.getElementById('quiz_questions').style.visibility = 'hidden';
+
+    // Add event listeners to multiple choice answer options
+    const answerElements = document.getElementsByClassName("answer");
+    for (let i = 0; i < answerElements.length; i++) {
+        answerElements[i].addEventListener('click', clickAnswer);
+    }
+});
+
+// Function to start the quiz
+function startQuiz() {
+    index = 0;
+    highscore = 0;
+    numberQuestions = parseInt(document.getElementById('noOfQuestions').value, 10); // Update number of questions here
+
+    document.getElementById('quiz_questions').style.visibility = 'inherit';
+    document.getElementById('quiz_settings').style.visibility = 'hidden';
+
+    shuffle(CountriesCapitals);
+    console.log(CountriesCapitals);
+
+    saveModeSettings();
+    console.log(modeTypeQuestion);
+    console.log(numberQuestions);
+
+    displayNextQuestion();
 }
 
-let highscore = 0;
-document.getElementById("highscore").innerHTML = "Highscore is: " + highscore;
+// Function to handle answer clicks
+function clickAnswer(event) {
+    const selectedAnswer = event.target.textContent;
+    handleUserAnswer(selectedAnswer);
+}
+
+// Function to handle the user's answer
+function handleUserAnswer(answer) {
+    userAnswer = answer;
+    checkAnswer();
+    // After checking the answer, proceed to the next question
+    displayNextQuestion();
+}
+
+// Function to display the next question or end the quiz if all questions have been answered
+function displayNextQuestion() {
+    if (index < numberQuestions) {
+        displayQuestionAnswer();
+        console.log('displayNextQuestion');
+        index++;
+        console.log('display index:' + index);
+        console.log('highscore:' + highscore);
+    } else {
+        // No more questions left, end of the game
+        gameOver();
+    }
+}
+
+// Function to handle the user's input
+function handleUserInput(userInput) {
+    userAnswer = userInput;
+    checkAnswer();
+    // After checking the answer, proceed to the next question
+    displayNextQuestion();
+}
 
 // Function to shuffle arrays, according to Fisher-Yates shuffle algorithm
 function shuffle(array) {
@@ -26,94 +158,116 @@ function shuffle(array) {
     }
 }
 
-let correctAnswer;
+function saveModeSettings() {
+    // Get the checkbox
+    var checkBoxQ = document.getElementById('CheckboxQuestion');
 
-// Function to generate a question
-function generateQuestion() {
-
-    // Generate a random index to select a country
-    let randomIndex = Math.floor(Math.random() * allCountriesCapitals.country.length);
-
-    // Get the random country using the random index
-    let randomCountry = allCountriesCapitals.country[randomIndex];
-
-    document.getElementById("box_questions").innerHTML = "What is the capital of " + randomCountry + " ?";
-
-    // Define correct Answer
-    correctAnswer = allCountriesCapitals.capital[randomIndex];
-
-    // using the modulo operator % to wrap around to the beginning of the array if the index exceeds its length
-    let wrongAnswer1 = allCountriesCapitals.capital[(randomIndex + 1) % allCountriesCapitals.capital.length];
-    let wrongAnswer2 = allCountriesCapitals.capital[(randomIndex + 2) % allCountriesCapitals.capital.length];
-
-    let allAnswers = [correctAnswer, wrongAnswer1, wrongAnswer2];
-
-    // shuffle through list allAnswers that that the right answer is not always on the same index
-    shuffle(allAnswers);
-
-    // provide input for answers in HTML
-    document.getElementById("box1").innerHTML = allAnswers[0];
-    document.getElementById("box2").innerHTML = allAnswers[1];
-    document.getElementById("box3").innerHTML = allAnswers[2];
-
+    // Show the status of the checkbox
+    if (checkBoxQ.checked == true) {
+        console.log('Question checkbox is checked');
+        modeTypeQuestion = "capital";
+    } else {
+        console.log('Question checkbox is unchecked');
+        modeTypeQuestion = "country";
+    }
 }
 
-// Provide feedback for user
-function clickAnswer(event) {
-    let userAnswer = event.target.textContent;
+// Show question depending on the setting choice
+function displayQuestionAnswer() {
+    switch (modeTypeQuestion) {
+        case 'capital':
+            displayQuestionCapital();
+            displayMultipleChoice();
+            console.log('capital');
+            console.log('Answer User:' + userAnswer);
+            console.log('Input User:' + userInput);
+            break;
+        case 'country':
+            displayQuestionCountry();
+            displayMultipleChoice();
+            console.log('country');
+            console.log('Answer User:' + userAnswer);
+            console.log('Input User:' + userInput);
+    }
+}
 
-    // check answer
+// Show question and define the right capital according to the index 
+function displayQuestionCapital() {
+    const currentQuestion = CountriesCapitals[index];
+
+    document.getElementById("box_questions").innerHTML = "What is the capital of " + currentQuestion.country + " ?";
+    correctAnswer = currentQuestion.capital;
+    console.log('correct answer defined. It is ' + correctAnswer);
+}
+
+// Show question and define the right country according to the index
+function displayQuestionCountry() {
+    const currentQuestion = CountriesCapitals[index];
+
+    document.getElementById("box_questions").innerHTML = currentQuestion.capital + " is the capital of which country?";
+    correctAnswer = currentQuestion.country;
+    console.log('correct answer defined. It is ' + correctAnswer);
+}
+
+function displayMultipleChoice() {
+    // Using the modulo operator % to wrap around to the beginning of the array if the index exceeds its length
+    if (modeTypeQuestion == "country") {
+        wrongAnswer1 = CountriesCapitals[(index + 1) % CountriesCapitals.length].country;
+        wrongAnswer2 = CountriesCapitals[(index + 2) % CountriesCapitals.length].country;
+    } else if (modeTypeQuestion === "capital") {
+        wrongAnswer1 = CountriesCapitals[(index + 1) % CountriesCapitals.length].capital;
+        wrongAnswer2 = CountriesCapitals[(index + 2) % CountriesCapitals.length].capital;
+    }
+
+    let allAnswers = [correctAnswer, wrongAnswer1, wrongAnswer2];
+    console.log(allAnswers);
+
+    // Shuffle through list allAnswers so that the right answer is not always on the same index
+    shuffle(allAnswers);
+
+    // Provide input for answers in HTML
+    answer1.innerHTML = allAnswers[0];
+    answer2.innerHTML = allAnswers[1];
+    answer3.innerHTML = allAnswers[2];
+}
+
+// Check answer
+function checkAnswer() {
     if (userAnswer === correctAnswer) {
         document.getElementById("feedbackForUser").innerHTML = "Correct!";
         highscore++;
         document.getElementById("highscore").innerHTML = "Highscore is: " + highscore;
+        console.log('check is done.')
     } else {
         document.getElementById("feedbackForUser").innerHTML = "Incorrect! The correct answer is " + correctAnswer;
-        gameOver();
     }
-
-    generateQuestion(); // Generate a new question after each answer
-
-}
-
-// Generate the first question
-generateQuestion();
-
-// Get all elements with the class name "answer"
-let answerElements = document.getElementsByClassName("answer");
-
-// Loop through each answer element and add event listener to it
-for (let i = 0; i < answerElements.length; i++) {
-    answerElements[i].addEventListener('click', clickAnswer);
 }
 
 // The game is over: Alert, add highscore to highscoreList, not working yet !!!!
-function gameOver () {
-    alert('The game is over. Your highscore is '  + highscore)
-    highscoreList.user('Tom');
-    highscoreList.highscore(highscore);
+function gameOver() {
+    console.log('highscore is: ' + highscore);
+    alert('The game is over. Your highscore is: ' + highscore);
+    localStorage.setItem("mostRecentScore", highscore);
+    window.location.replace('/end.html');
 }
 
+function saveHighScore(e) {
+    console.log("clicked the save button!");
+}
 
-/* following function planned:
-loop while for 
-    generateQuestion()
-    addHIghscore to Object or array
+/* TIMER
+var timeLeft = 30;
+var elem = document.getElementById('Timer');
 
-ChangeMode_CapitalCountry()
+var timerId = setInterval(countdown, 1000);
 
-normalQuestionOneAnswer()
-
-activateMultipleChoice()
-
-ShowHighscoreList()
-
-addUserName()
-
-
-
-
--- change array to object CountryCapital
--- add background image of city in list
--- add president, cancelor, king, ... 
--- add currency */
+function countdown() {
+  if (timeLeft == 0) {
+    clearTimeout(timerId);
+    gameOver();
+  } else {
+    elem.innerHTML = timeLeft + ' seconds remaining';
+    timeLeft--;
+  }
+}
+*/
